36eee44ab23e03d0f7ff3de81c8c6188c56ddc6e
Zecksans <ezequielsantosdsilva@gmail.com>
2024-05-24 23:49:01 -0300

    Commit aula Operador de Atribuição.

---
+++ INTRODUCAO-JAVA/SINTAXE-JAVA/src/edu/ezequiel/operadores/OperadorAtribuicao.java
@@ -0,0 +1,32 @@
+package edu.ezequiel.operadores;
+
+public class OperadorAtribuicao {
+    
+    public static void main(String[] args) {
+   
+    /* ATRIBUIÇÃO.
+    Representado pelo símbolo de igualdade "=".
+
+    O operador de atribuição é utilizado para definir o valor inicial ou 
+    sobrescrever o valor de uma variável. em Java, 
+    definimos um tipo, nome e opcionalmente atribuímos um valor à variável 
+    através do operador de atribuição. Exemplos abaixo:
+    */
+        //classe Operadores.java
+        String nome = "EZEQUIEL ";
+        String sobrenome = "SANTOS ";
+        int idade = 22;
+        double peso = 68.5;
+        char sexo = 'M';                  // char, aceita apenas um caractere.
+        boolean doadorOrgao = false;
+        
+        System.out.println(nome);
+
+
+
+
+
+
+
+    }
+}
