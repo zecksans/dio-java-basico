cb4d1ada10ce987d0843713a67c35f6fe1e84e81
Zecksans <ezequielsantosdsilva@gmail.com>
2024-05-29 23:40:39 -0300

    Commit Alterações na Class Usuário.

--- INTRODUCAO-JAVA/PROJETOS-JAVA/sistema-smart-tv/src/Usuario.java
+++ INTRODUCAO-JAVA/PROJETOS-JAVA/sistema-smart-tv/src/Usuario.java
@@ -1,5 +1,11 @@
 public class Usuario {
     public static void main(String[] args) throws Exception {
         
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " + smartTv.volume);
+    
     }
 }
