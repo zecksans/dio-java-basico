864010586c26903c8634ae89b202a10d30882901
Zecksans <ezequielsantosdsilva@gmail.com>
2024-05-29 23:44:20 -0300

    Commit criando os métodos ligar e desligar.

--- INTRODUCAO-JAVA/PROJETOS-JAVA/sistema-smart-tv/src/SmartTv.java
+++ INTRODUCAO-JAVA/PROJETOS-JAVA/sistema-smart-tv/src/SmartTv.java
@@ -3,4 +3,13 @@ public class SmartTv {
     int canal = 1;
     int volume = 15;
    
+    public void ligar(){
+    ligada = true;
+    }
+
+    public void desligar(){
+        ligada = false;
+    }
+
+
 }
