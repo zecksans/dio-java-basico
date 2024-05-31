9968d28317de42ea73d3bad741ec543d86778902
Zecksans <ezequielsantosdsilva@gmail.com>
2024-05-30 23:19:11 -0300

    Commit criando a Class Comentario dentro de documentacao

---
+++ INTRODUCAO-JAVA/SINTAXE-JAVA/src/edu/ezequiel/documentacao/Comentario.java
@@ -0,0 +1,25 @@
+package edu.ezequiel.documentacao;
+
+public class Comentario {
+    
+    public static void main(String[] args) {
+        
+        // Olá, eu sou um comentário em uma única linha
+
+        /* Olá,
+        * Eu sou um comentário
+        * que posso ser mais detalhado
+        * quando necessário
+        */  
+
+        /** 
+         * Estas duas estrelinhas acima
+         * é para identificar que você
+         * pretende elaborar um comentário
+         * a nível de documentação.
+         * Que incrível !!!
+         */
+
+    }
+
+}
