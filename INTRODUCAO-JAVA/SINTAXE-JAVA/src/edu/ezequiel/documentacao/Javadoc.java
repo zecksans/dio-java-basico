327a965178d8c2998e6a1dac7bb849d83356bf4a
Zecksans <ezequielsantosdsilva@gmail.com>
2024-05-30 23:52:42 -0300

    Commit criando a Class Javadoc

---
+++ INTRODUCAO-JAVA/SINTAXE-JAVA/src/edu/ezequiel/documentacao/Javadoc.java
@@ -0,0 +1,21 @@
+package edu.ezequiel.documentacao;
+
+public class Javadoc {
+    
+    public static void main(String[] args) {
+        
+/*JAVADOC
+Javadoc é um gerador de documentação criado pela Sun Microsystems ,
+para documentar a API dos programas em Java, a partir do código-fonte.
+O resultado é expresso em HTML. É constituído, basicamente, por algumas marcações muitos simples,
+inseridas nos comentários do programa.
+Este sistema, é o padrão de documentação de classes em Java, 
+onde muitas das IDEs desta linguagem irão automaticamente gerar um Javadoc em HTML. */
+
+//Criando nossa documentação no formato html, para disponibilizar via web.
+
+// No terminal execute o comando abaixo
+
+//javadoc -encoding UTF-8 -docencoding ISO-8859-1  -d ../docs  src/*.java
+    }
+}
