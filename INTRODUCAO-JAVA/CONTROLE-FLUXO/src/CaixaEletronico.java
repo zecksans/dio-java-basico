f3866b0b3d545f90636e112cff336fe94be51d18
Zecksans <ezequielsantosdsilva@gmail.com>
2024-06-01 22:43:54 -0300

    Criando o projeto de estudos Controle de fluxos

--- INTRODUCAO-JAVA/CONTROLE-FLUXO/src/CaixaEletronico.java
+++ INTRODUCAO-JAVA/CONTROLE-FLUXO/src/CaixaEletronico.java
@@ -3,7 +3,7 @@ public class CaixaEletronico {
     public static void main(String[] args) {
         
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 27.0;
 
         if(valorSolicitado < saldo)
             saldo = saldo - valorSolicitado;
