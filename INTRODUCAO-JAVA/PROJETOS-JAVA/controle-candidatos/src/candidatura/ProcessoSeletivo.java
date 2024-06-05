a515558dd6c5787254756aed68053a0d19e0b9e5
Zecksans <ezequielsantosdsilva@gmail.com>
2024-06-05 01:03:20 -0300

    Criando um método imprimirSelecionados

--- INTRODUCAO-JAVA/PROJETOS-JAVA/controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ INTRODUCAO-JAVA/PROJETOS-JAVA/controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -5,7 +5,21 @@ public class ProcessoSeletivo {
     public static void main(String[] args) {
         System.out.println("Processo seletivo");
         selecaoCandidatos();
+        imprimirSelecionados();
 
+    }
+
+    static void imprimirSelecionados(){
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+        for(int indice = 0; indice < candidatos.length; indice++) {
+            System.out.println("O candidato de numero " + (indice+1) + " é " + candidatos[indice]);
+        }
+
+        System.out.println("Forma abreviada de interação for each");
+        for(String candidato: candidatos) {
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
     }
 
     static void selecaoCandidatos(){
