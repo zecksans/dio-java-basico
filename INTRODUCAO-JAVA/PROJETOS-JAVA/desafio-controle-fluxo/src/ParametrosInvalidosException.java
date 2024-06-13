04545218ef8f125616e2b69e7b32250cc84f8887
Zecksans <ezequielsantosdsilva@gmail.com>
2024-06-13 01:43:51 -0300

    Alterando nomenclatura pasta

---
+++ INTRODUCAO-JAVA/PROJETOS-JAVA/desafio-controle-fluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+// Criando uma excecão checada
+public class ParametrosInvalidosException extends Exception{
+ 
+    public ParametrosInvalidosException(String message) {
+  
+        super(message);
+        }
+}
